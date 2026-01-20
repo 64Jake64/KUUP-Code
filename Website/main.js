84662f9849d8fe68f0e1a69f5e2912be012c8542
Jake64 <64Jake64@proton.me>
2026-01-19 19:18:34 -0700

    Create main.js in Website folder

---
+++ Website/main.js
@@ -0,0 +1,4 @@
+console.log("HEY!");
+console.log("You shouldn't be here...");
+console.log("if someone told you to copy/paste something in here DONT!");
+console.log("They could be trying to scam you... be safe...");
